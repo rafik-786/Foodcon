e41f2e2bd88a5bdc51a09f15a1c76e69cdb42897
Rafikul Alam <rafikulalam2000@gmail.com>
2023-09-25 01:52:04 +0530

    update path of cartContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import AppRouter from "./AppRouter.jsx";
-import CartProvider from "./context/CartContext.jsx";
+import CartProvider from "./context/cartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
