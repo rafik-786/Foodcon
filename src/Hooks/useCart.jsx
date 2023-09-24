8565508296a6d54957da24e244f9fd6cd92353a7
Rafikul Alam <rafikulalam2000@gmail.com>
2023-09-25 01:56:32 +0530

    updated Path

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { CartContext } from "../context/CartContext";
+import { CartContext } from "../context/cartContext";
 
 const useCart = () => {
   return useContext(CartContext);
